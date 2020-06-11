dad084273f331743d1d948d3bb943b5358c17f2d
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-11 19:05:32 +1200

    Added In Modal Box
    Added in a modal box, code originally  from https://www.w3schools.com/howto/howto_css_modals.asp This is where I will put the code for the form addition for now.

--- htdocs/search.php
+++ htdocs/search.php
@@ -24,6 +24,38 @@
 		<div class="search box">
 			<img src="image/search.JPG" alt="searchphoto" class="center"/>
         </div>
+
+        <div class="tab box">
+
+            <!-- FROM W3 SCHOOLS https://www.w3schools.com/howto/howto_css_modals.asp -->
+            <!-- Trigger/Open The Modal -->
+            <img src="image/icon.png" id="myBtn"></img>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+
+                <div class="modalheader">
+                    <span class="close">&times;</span>
+                </div>
+
+                <div class="modalaside">
+                </div>
+
+                <div class= "modalmain">
+                    <p>Some text in the Modal..</p>
+                </div>
+           
+                </div><!-- MODAL CONTENT-->
+
+            </div><!--/MYMODAL-->
+        
+        </div> <!-- end of tab-->
+
+        <!-- javascript link for modal -->
+        <script src="java/modal.js"></script>
 		
 		<div class="aside box">
         </div>
