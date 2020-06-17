2494efdb79e3b5d803177e8c7d706e4ea83a8251
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-17 20:23:10 +1200

    config setup
    Created new config branch to link to topbit.php so that db is connected to website.

---
+++ htdocs/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display errors', 'On') // change to 'Off when live
+
+define ("DB_NAME","epiz_25213442_XXX");
+define ("DB_USERNAME","epiz_25213442");
+define ("DB_PASSWORD","0viumg8yQekJ");
+define ("DB_HOST","sql306.epizy.com");
+
+?>
