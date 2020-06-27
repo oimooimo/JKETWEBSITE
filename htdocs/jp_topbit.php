af6fc52725f920f47be3539a7c5bf93ff9e119de
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-27 13:56:45 +1200

    Results VS2
    Version two of Results with Check In Yes or No and description

--- htdocs/jp_topbit.php
+++ htdocs/jp_topbit.php
@@ -1,35 +1,162 @@
-<!DOCTYPE html><!-- declares that the document is in html -->
-<html lang="ja">
+<?php include ("jp_topbit.php");
+    $find_sql = "SELECT * FROM `book_data`
+    JOIN Author ON (book_data.AuthorID = Author.AuthorID)
+    JOIN Item ON (book_data.ItemID = Item.ItemID)
+    JOIN Publisher ON (book_data.PubID = Publisher.PubID)
+    ";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+ ?>
 
-<?php
+    <link rel="stylesheet" href="css/searchstyle.css">
 
-    session_start(); //to allow variable transfer between pages....
-    include("config.php");
-    //connect to the database...
-    $dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-    if(mysqli_connect_errno()) {
-        echo "Connection failed:".mysqli_connect_error();
-        exit;
-    }
+ <?php include 'jp_regheader.php' ?>
+  <div> <a href="search.php" class="button_1 box">ENGLISH</a></div>
+    </div><!--closing for mobileheader--> 
+        
+        <header class="box">
 
-    mysqli_set_charset($dbconnect, 'utf8mb4'); //sets all data coming from database as utf8mb4
-    ?>
+            <div class="button_group box">
+				<a href="/search.php" class="button_1">ENGLISH</a>
+				<a href="jp_donation.php" class="button_2">ご寄付</a>
+				<a href="jp_volunteerform.php" class="button_2">ボランティア募集！</a>
+			</div>
 
+            <div class="header_item box">
+                <?php include'headeritem.php' ?>
+            </div>
 
-<head>
-    <!-- meta information -->
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content ="width=device-width, initial=scale=1.0">
-    
-    <meta name="description" content="The website for the Japan Kauri Education Trust">
-    <meta name="keywords" content="JKET, New Zealand,Japan Kauri Education Trust, Japan Heritage center, Japanese Childrens library, Japan, Japanese Classes">
-    <meta name="author" content="ガーネル華">
+        </header>
+		
+		<div class="search">
+			<img src="image/search.JPG" alt="searchphoto" class="center"/>
+        </div>
+		
+       <div class="tab box">
 
-    <title> Japan Kauri Education Trust </title>
-    <!-- Javascript from https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp for drop down button -->
-    <script src="java/dropdown.js" async></script>
-    <!-- Java script for the slide -->
-    <script src="java/slide.js" async></script>
-    <!-- end of Javascript-->
+            <!-- FROM W3 SCHOOLS https://www.w3schools.com/howto/howto_css_modals.asp -->
+            <!-- Trigger/Open The Modal -->
+            <img src="image/icon.png" id="myBtn"></img>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+
+                <div class="modalheader">
+                    <span class="close">&times;</span>
+                </div>
+
+                <div class="modalaside">
+                d
+                </div>
+
+                <div class= "modalmain">
+                    <p>Some text in the Modal..</p>
+                </div>
+           
+                </div><!-- MODAL CONTENT-->
+
+            </div><!--/MYMODAL-->
+        
+        </div> <!-- end of tab-->
+
+        <!-- javascript link for modal -->
+        <script src="java/modal.js"></script>
+
+        <div class="maincontent">
+            <div class="aside box">
+            </div>
+
+            <div class="content1 box">
+                <h2> Results </h2>
+
+                <?php 
+
+                if($count < 1) {
+
+                    ?>
+
+                <div class="error">
+
+                    Sorry! There are no results that match your search. Please use the search box in the right for a more detailed search!
+
+                </div><!-- end error -->
+
+                <?php
+                } // end no results if
+
+                else {
+                    do
+                    {
+                        ?>
+                        
+                <!-- Results go here -->
+                <div class="results">
+                
+                <!-- title and check in-->
+                <div class="flex-container">
+                        <div>
+                            <span class="sub_heading">
+                                <?php echo $find_rs['Title']; ?>
+                            </span>
+                        </div> 
+                    <!--/title-->
+
+                    <!--Availability-->
+                    
+                    <?php 
+                        if ($find_rs['CheckIn'] == 0) {?>
+                            Available! 
+
+                        <?php
+                        } //end Check in if
+
+                        else {
+
+                        ?> Checked out
+                        <?php
+                        } // end check in else
+
+                        ?>
+                    
+                    <!--/availability-->
+                </div> <!--/flexcontainer-->
+
+                <div>
+                    <b>Item:</b>
+                    <?php echo $find_rs['Item']; ?>
+
+                    <br />
+                    <b> Author:</b>
+                    <?php echo $find_rs['Author']; ?>
+
+                    <br />
+                    <b> Publisher:</b>
+                    <?php echo $find_rs['Pub']; ?>
+                </div> <!--- Sub information-->
+                    <hr />
+                     
+                <div>
+                    <b> Description:</b>
+                    <?php echo $find_rs['Description']; ?>
+                </div><!---Description-->
+
+                </div> <!-- end of results -->
+                <?php
+
+                    } // end results 'do'
+
+                    while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                } // end else
+
+                ?>
+
+            </div> <!-- end of content1 -->
+
+        </div> <!-- end of main-->
+
+<?php include 'jp_bottombit.php'?>
