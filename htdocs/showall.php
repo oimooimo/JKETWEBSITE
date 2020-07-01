7f154291b03459ab3f70dc6c28f9d2193a2f24e4
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-01 20:01:42 +1200

    English version of search v2

---
+++ htdocs/showall.php
@@ -0,0 +1,104 @@
+<?php include ("topbit.php");
+
+    $result = TRIM($_POST['searchbasic']);
+    
+    $find_sql = "SELECT * FROM `book_data`
+    JOIN Author ON (book_data.AuthorID = Author.AuthorID)
+    JOIN Item ON (book_data.ItemID = Item.ItemID)
+    JOIN Publisher ON (book_data.PubID = Publisher.PubID)
+    WHERE `Title` LIKE '%$result%' OR
+    `Description` LIKE '%$result%' OR
+    `Author` LIKE '%$result%' OR
+    `KatAuthor` LIKE '%$result%' OR
+    `RomAuthor` LIKE '%$result%' OR
+    `Pub` LIKE '%$result%' OR
+    `KatPub` LIKE '%$result%' OR
+    `RomPub` LIKE '%$result%' OR
+    `Item` LIKE '%$result%' OR
+    `RomItem` LIKE '%$result%' OR
+    `KatItem` LIKE '%$result%'
+    "; 
+
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+ ?>
+
+    <link rel="stylesheet" href="css/searchstyle.css">
+
+ <?php include 'regheader.php' ?>
+       <div> <a href="jp_showall.php" class="button_1 box">日本語</a></div>
+    </div><!--closing for mobileheader--> 
+        
+        <header class="box">
+
+            <div class="button_group box">
+				<a href="/jp_showall.php" class="button_1">日本語</a>
+				<a href="/donation.php" class="button_2">Donate!</a>
+				<a href="/volunteerform.php" class="button_2">Volunteer!</a>
+			</div><!-- end of button_group-->
+
+
+            <div class="header_item box">
+                <?php include'headeritem.php' ?>
+            </div><!-- end of header_item-->
+            
+        </header>
+		
+		<div class="search">
+        <img src="image/search.JPG" alt="searchphoto" class="center"/>
+        
+        <form class="searchform" method="post" action="jp_showall.php" enctype="multipart/form-data">
+        
+            <input class="searchbox" type="text" name="searchbasic" size="80" value="" />
+            <input class="submit" type="submit" name="findsearch" value="&#8981;"/>
+		</form>	
+        </div>
+		
+       <div class="tab box">
+
+            <!-- FROM W3 SCHOOLS https://www.w3schools.com/howto/howto_css_modals.asp -->
+            <!-- Trigger/Open The Modal -->
+            <img src="image/icon.png" id="myBtn"></img>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+
+                <div class="modalheader">
+                    <span class="close">&times;</span>
+                </div>
+
+                <div class="modalaside">
+                d
+                </div>
+
+                <div class= "modalmain">
+                    <p>Some text in the Modal..</p>
+                </div>
+           
+                </div><!-- MODAL CONTENT-->
+
+            </div><!--/MYMODAL-->
+        
+        </div> <!-- end of tab-->
+
+        <!-- javascript link for modal -->
+        <script src="java/modal.js"></script>
+
+        <div class="maincontent">
+            <div class="aside box">
+            </div>
+
+            <div class="content1 box">
+                <h2> Results </h2>
+
+               <?php include 'jp_results.php'?>
+
+            </div> <!-- end of content1 -->
+
+        </div> <!-- end of main-->
+
+<?php include 'jp_bottombit.php'?>
