81f095cad9ca1e03b2e2e5534c6e9d5dac019c57
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-02 18:50:02 +1200

    Advanced search vs1

--- htdocs/search.php
+++ htdocs/search.php
@@ -1,32 +1,46 @@
-<?php include ("topbit.php");
-    $find_sql = "SELECT * FROM `book_data`";
+<?php include ("jp_topbit.php");
+    $find_sql = "SELECT * FROM `book_data`
+    JOIN Author ON (book_data.AuthorID = Author.AuthorID)
+    JOIN Item ON (book_data.ItemID = Item.ItemID)
+    JOIN Publisher ON (book_data.PubID = Publisher.PubID)
+    ";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
     $count = mysqli_num_rows($find_query);
  ?>
-
     <link rel="stylesheet" href="css/searchstyle.css">
 
- <?php include 'jp_regheader.php' ?>
-  <div> <a href="search.php" class="button_1 box">ENGLISH</a></div>
+ <?php include 'regheader.php' ?>
+       <div> <a href="jp_search.php" class="button_1 box">日本語</a></div>
     </div><!--closing for mobileheader--> 
         
         <header class="box">
 
             <div class="button_group box">
-				<a href="/search.php" class="button_1">ENGLISH</a>
-				<a href="jp_donation.php" class="button_2">ご寄付</a>
-				<a href="jp_volunteerform.php" class="button_2">ボランティア募集！</a>
-			</div>
+				<a href="/jp_search.php" class="button_1">日本語</a>
+				<a href="/donation.php" class="button_2">Donate!</a>
+				<a href="/volunteerform.php" class="button_2">Volunteer!</a>
+			</div><!-- end of button_group-->
+
 
             <div class="header_item box">
                 <?php include'headeritem.php' ?>
-            </div>
-
+            </div><!-- end of header_item-->
+            
         </header>
 		
-		<div class="search">
-			<img src="image/search.JPG" alt="searchphoto" class="center"/>
+
+<div class="search">
+        <img src="image/search.JPG" alt="searchphoto" class="center"/>
+        
+        <form class="searchform" method="post" action="showall.php" enctype="multipart/form-data">
+        
+            <div class="basicsearch">
+            <input class="searchbox" type="text" name="searchbasic" size="80" value="" required placeholder=""/>
+            <input class="submit" type="submit" name="findsearch" value="&#8981;"/>
+            </div><!-- /basic search--->
+
+		</form>	
         </div>
 		
        <div class="tab box">
@@ -63,48 +77,29 @@
         <script src="java/modal.js"></script>
 
         <div class="maincontent">
+
             <div class="aside box">
-            </div>
+                <h2>Advanced Search</h2>
+
+                <div class="advanced-frame">
+                
+                <form class="searchform" method="post" action="advanced.php" encytype="multipart/form-data">
+                <p> form goes here<p>
+                <input class="submit advanced-button" type="submit" name="advanced" value="Search &#8981;" />
+                </form>
+                </div> <!--advanced frame-->
+            </div> <!--/aside box-->
 
             <div class="content1 box">
-                <h2> Results </h2>
+                <h2> Book Search </h2>
 
-                <?php
-
-                if($count < 1) {
-
-                    ?>
-
-                <div class="error">
-
-                    Sorry! There are no results that match your search. Please use the search box in the right for a more detailed search!
-
-                </div><!-- end error -->
-
-                <?php
-                } // end no results if
-
-                else {
-                    do
-                    {
-                        ?>
-                        
-                <!-- Results go here -->
-                <div class="results">
-                    <?php echo $find_rs['Title']; ?>
-
-                </div> <!-- end of results -->
-                <?php
-
-                    } // end results 'do'
-
-                    while
-                    ($find_rs=mysqli_fetch_assoc($find_query));
-                } // end else
-
-                ?>
+               <?php include 'results.php'?>
 
             </div> <!-- end of content1 -->
+
+            </div>
+
+            
 
         </div> <!-- end of main-->
 
