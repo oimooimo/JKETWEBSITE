af1d62b86523e7d8f91a68467ebde8641d17abfc
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-14 09:04:59 +1200

    Add entry vs2
    All of the spaces for the different values added, now need to connect to database to populate the Author, Publisher and item type

--- htdocs/addentry.php
+++ htdocs/addentry.php
@@ -1,11 +1,11 @@
-
 <?php 
 
 //initialise form variables
 
 $book_title="";
 $author="";
-$pub="";
+$AuthorID="";
+$Pub="";
 $item_type="";
 $pubyear="";
 $description="";
@@ -13,7 +13,8 @@ $isbn="";
 $check_in= 1;
 
 $has_errors = "no";
-
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            echo "you pushed the button";}
 ?>
 
         <div class="add-entry">
@@ -24,12 +25,62 @@ $has_errors = "no";
 
                 <!-- Book Name (Required) -->
                 <input class="add-field" type="text" name="book_title" value="<?php echo $app_name; ?>" required placeholder="Book Title (required)..."/>
-                <!-- Author Name (Required) -->
-                <!-- Publisher Name (Required) -->
-                <!-- Item Name (Required) -->
+                <!-- Author dropdown Name (Required) -->
+                    <select class="adv" required name="author">
+                    <!-- first/selected option-->
+                        <option value="" selected>Author (choose something....)</option>
+                    <!-- getting options from the database -->
+                    <?php
+                    do{
+                        ?>
+
+                    <option value="<?php echo $author_rs['AuthorID'];?>"><?php echo $author_rs['Author'];?></option>
+
+                    <?php
+                    }
+                    while ($author_rs=mysqli_fetch_assoc($author_query))
+                    ?>
+                    </select>
+                    
+                <!-- Publisher Name dropdown (Required) -->
+                  <select class="adv" required name="publisher">
+                <!-- first/selected option-->
+                    <option value="" selected>Publisher (choose something....)</option>
+                <!-- getting options from the database -->
+                <?php
+                do{
+                    ?>
+
+                <option value="<?php echo $publisher_rs['PubID'];?>"><?php echo $publisher_rs['Pub'];?></option>
+
+                <?php
+                }
+                while ($publisher_rs=mysqli_fetch_assoc($pub_query))
+                ?>
+                </select>
+                <!-- Item Name dropdown (Required) -->
+                      <select class="adv" required name="item">
+                    <!-- first/selected option-->
+                    <!-- first/selected option-->
+                    <option value="" selected>item type (choose something....)</option>
+                    <!-- getting options from the database -->
+                    <?php
+                    do{
+                        ?>
+
+                    <option value="<?php echo $item_rs['itemID'];?>"><?php echo $item_rs['item'];?></option>
+
+                    <?php
+                    }
+                    while ($item_rs=mysqli_fetch_assoc($item_query))
+                    ?>
+                    </select>
                 <!-- Published Year (Required) -->
+                <input class="add-field" type="number" name="pubyear" value="<?php echo $pubyear; ?>" required minlength="4" maxlength="4" placeholder="Pub Year (required)..."/>
+                
                 <!-- ISBN  (Required) -->
-                <!-- Book Name (Required) -->
+                <input class="add-field" type="number" name="isbn" value="<?php echo $isbn; ?>" required minlength="13" maxlength="13" minvalue="9780000000000" maxvalue="9800000000000" placeholder="ISBN 13 number"/>
+
                 <!-- SUbmit Button-->
                 <p> 
                 <input class="submit advanced-button" type="submit" value="Submit"/>
