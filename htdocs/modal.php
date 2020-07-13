fd1db483640fa05f5b9af2d3828c2edf34a3384a
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-14 09:01:51 +1200

    Create modal.php
    Created modal php so i didnt have to edit every page, helps prevent errors

---
+++ htdocs/modal.php
@@ -0,0 +1,32 @@
+ <div class="tab box">
+
+            <!-- FROM W3 SCHOOLS https://www.w3schools.com/howto/howto_css_modals.asp -->
+            <!-- Trigger/Open The Modal -->
+            <img src="image/icon.png" id="myBtn"></img>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+
+                <div class="modalheader">
+                    <span class="close">&times;</span>
+                </div>
+
+                <div class="modalaside">
+                d
+                </div>
+
+                <div class= "modalmain">
+                <?php include 'addentry.php'?>
+                </div>
+           
+                </div><!-- MODAL CONTENT-->
+
+            </div><!--/MYMODAL-->
+        
+        </div> <!-- end of tab-->
+
+        <!-- javascript link for modal -->
+        <script src="java/modal.js"></script>
