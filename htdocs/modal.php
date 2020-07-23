57b551daf8d4e7d0bbd69e3dc8c9f53fe0f0eeec
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-23 16:28:37 +1200

    Modal update
    Modal linked currently to admin.php for the time being until accounts made

--- htdocs/modal.php
+++ htdocs/modal.php
@@ -1,31 +1,41 @@
- <div class="tab box">
+            <div class="tab box">
 
             <!-- FROM W3 SCHOOLS https://www.w3schools.com/howto/howto_css_modals.asp -->
             <!-- Trigger/Open The Modal -->
-            <img src="image/icon.png" id="myBtn"></img>
-
+                        <a href="admin.php">
+                        <img src="image/icon.png"></img> <!-- id="myBtn" href="search.php"-->
+                        </a>
             <!-- The Modal -->
-            <div id="myModal" class="modal">
+                        <div id="myModal" class="modal modal-dialog modal-dialog-centered"">
 
-                <!-- Modal content -->
-                <div class="modal-content">
+                            <!-- Modal content -->
+                            <div class="modal-content">
 
-                <div class="modalheader">
-                    <span class="close">&times;</span>
-                </div>
+                                <div class="modalheader">
+                                    <span class="close">&times;</span>
+                                </div>
 
-                <div class="modalaside">
-                d
-                </div>
+                                <div class="modalaside">
+                                    <div id="tabs">
+                                        <a class="active" href="#Menu1">Book Addition</a> 
+                                    </div>
+                                </div>
 
-                <div class= "modalmain">
-                <?php include 'addentry.php'?>
-                </div>
-           
-                </div><!-- MODAL CONTENT-->
+                                <div id="tabcontent" class= "modalmain">
+                                    <div id="Menu1">
+                                        <?php include 'addentry.php'?>
+                                    </div>
 
-            </div><!--/MYMODAL-->
-        
+                                    <div id="Menu2">
+                                        HI
+                                    </div>
+                                </div>
+
+                    
+                            </div><!-- MODAL CONTENT-->
+
+                        </div><!--/MYMODAL-->
+                    
         </div> <!-- end of tab-->
 
         <!-- javascript link for modal -->
