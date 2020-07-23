1dd9a5812d91669ed1e294a0d26ee87bb32e7bdb
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-23 16:31:05 +1200

    add_success.php
    Links back to admin.php once entry is succesfully submited

---
+++ htdocs/add_success.php
@@ -0,0 +1,32 @@
+<?php include ("topbit.php");?>
+    <link rel="stylesheet" href="css/loginstyle.css">
+<div class="wrapper">
+<div class="search">
+        <img src="image/search.JPG" alt="searchphoto" class="center"/>
+
+        </div>
+            <div class="menu box">
+
+                        <a href="search.php">
+                        <img src="image/icon.png" id="myBtn" href="search.php"></img>
+                        </a>
+            </div>		
+            <div class="maincontent">
+
+            <div class="aside box">
+
+            </div> <!--/aside box-->
+
+            <div class="content1 box">
+                <div id="tabcontent" class= "modalmain">
+                    <div class="active" id="Menu1">
+                <h2>Congratulations</h2>
+                 You have succesfully submited an entry into the Database.
+                <a href="admin.php">Please click here to go back</a>
+            </div> <!-- end of content1 -->
+
+        </div> <!-- end of main-->
+    </div>
+
+</body>
+</html>
