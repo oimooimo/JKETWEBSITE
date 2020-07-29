95e01aa946a9e585ac33b0d1a9198e7eac85c327
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-29 19:02:07 +1200

    Login vs 2
    By putting another if clause inside of the clause checking if both passwords usernames match each other. The User login now displays No instead of taking users to the admin page

--- htdocs/adminlogin.php
+++ htdocs/adminlogin.php
@@ -5,16 +5,20 @@ if(isset ($_REQUEST['login'])){
     $login_sql="SELECT * FROM `Users` WHERE Username='".$_POST['username']."' AND Password='".$_POST['password']."'";
     $login_query=mysqli_query($dbconnect,$login_sql);
 
-    if(mysqli_num_rows($login_query)>0||$username == "admin" )
+    // if user is admin then take them to admin panel and give their session admin
+    if((mysqli_num_rows($login_query)>0))
     {
+        if($username === "Admin"){
         $_SESSION['admin'];
         header('location:admin.php');
+        }
+        //if user is not an admin then give them a user session and take them to user panel
+        else{
+            echo "No";
+        }
     }
 
-
-    elseif(mysqli_num_rows($login_query)>0){
-        echo "No";
-    }
+        
     else{
         echo "Error";
     }
