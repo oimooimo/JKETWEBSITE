e40435cd2755bfe5ff608ae12aca127162a07b64
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-07-31 13:04:52 +1200

    login 3.5
    Admin login now depends on the account type not on the username

--- htdocs/adminlogin.php
+++ htdocs/adminlogin.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 //define variables
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -20,30 +20,35 @@ $passerror = "";
         $pass_field="form-error";
     }
 
-if(isset ($_REQUEST['login'])){
+    if(isset ($_REQUEST['login'])){
 
-    $login_sql="SELECT * FROM `Users` WHERE Username='".$_POST['username']."' AND Password='".$_POST['password']."'";
-    $login_query=mysqli_query($dbconnect,$login_sql);
+        $login_sql="SELECT * FROM `Users` WHERE Username='$username' AND Password='$password'";
+        $login_query=mysqli_query($dbconnect,$login_sql);
 
-    // if user is admin then take them to admin panel and give their session admin
-    if((mysqli_num_rows($login_query)>0)&& ($has_errors=="no"))
-    {
-        if($username === "Admin"){
-        $_SESSION['admin'];
-        header('location:admin.php');
-        }// end of if
+        // if user is admin then take them to admin panel and give their session admin // mrs greenwood from here  https://stackoverflow.com/questions/22340304/how-to-make-login-form-with-admin-and-user-in-php/22340649
+        if((mysqli_num_rows($login_query)>0)&& ($has_errors=="no")){
+            
 
-        //if user is not an admin then give them a user session and take them to user panel
-        else{
-            $_SESSION['user'];
-            echo "No";
-        }// end of else
+                $login_row=mysqli_fetch_array($login_query);
+                    if ($login_row['Account']== 1){
+                    $_SESSION["login"]= "admin";
+                    header('location:admin.php');
+                    }// end of if
 
-    }//end of main if
+                //if user is not an admin then give them a user session and take them to user panel
+                else{
+                    $_SESSION['login']= "user";
+                    echo "No";
+                }// end of else
 
-    else{ //pasword error
-      $passerror="incorrect username and/or password";
+        }//end of main if
+
+        else{ //pasword error
+        $passerror="incorrect username and/or password";
+        }
     }
-}
+
+
+
 
 ?>
