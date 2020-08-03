fa1bdc6cae3a2233bc733a688ee0a7a679aba49b
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-03 14:29:22 +1200

    User.php now user has to be logged in

--- htdocs/user.php
+++ htdocs/user.php
@@ -1,18 +1,19 @@
 <?php 
     session_start();
-    //if(($_SESSION["login"]) == "admin"){
-    //include ("topbit.php");
+    if(($_SESSION["login"]) == "admin"){
+    include ("topbit.php");
 ?>
      
-<?php include ("topbit.php");?>
     <link rel="stylesheet" href="/css/loginstyle.css">
 <div class="wrapper">
 <div class="search">
-        <img src="image/search.JPG" alt="searchphoto" class="center"/>
+        <img src="image/search.JPG" alt="searchphoto" style="height: 200px;" class="center"/>
 
         </div>
             <div class="menu box">
-                        <a href="<?php logout.php ?>"> Logout </a>
+                        
+                        <a href="login.php"> Logout </a>
+
                         <a href="search.php">
                         <img src="image/icon.png" id="myBtn" href="search.php"></img>
                         </a>
@@ -21,17 +22,25 @@
 
             <div class="aside box">
                 <div id="tabs">
-                <a class="tabs" href="#Menu1">Checked outs</a>
-                <a class="tabs" href="#Menu2">Account Details</a>
+                <a class="tabs" href="#Menu1">Book Addition</a>
+                <a class="tabs" href="#Menu2">User Addition</a>
+                <a class="tabs" href="#Menu3">User List</a>
+                <a class="tabs" href="#Menu4">Delete</a>
+
                 </div><!--tabs-->
             </div> <!--/aside box-->
 
             <div class="content1 box">
                 <div id="tabcontent">
                     <div class="tabcontent active" id="Menu1">
+                    <?php include 'addentry.php'?>
                     </div>
                     <div class="tabcontent" id="Menu2">
-                        <?php include 'userinfo.php' ?>
+                        <?php include 'adduser.php' ?>
+                    </div>
+
+                    <div class="tabcontent" id="Menu3">
+                        <?php include 'userlist.php' ?>
                     </div>
                 </div><!-- end of tB CONTENT    -->
 
@@ -42,3 +51,11 @@
 
 </body>
 </html>
+<?
+    }// end of Admin session if
+    else{
+        header('Location:login.php');
+         exit;
+
+    }
+    ?>
