62ba204ec32d67db57d68fcb8ff5c542bb283dce
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-03 18:58:06 +1200

    login vs 4.5
    Now when user logs in and they go back to login page, it will redirect them to the account that they are on. so when they go back ot the search and then click back on the profile, it will direct them directly to their profile instead of back to login.

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,3 +1,16 @@
+<?php 
+    session_start();
+        if($_SESSION['login']==='admin'){
+            header('Location:admin.php');
+        }
+
+        elseif($_SESSION['login']==='user'){
+            header('Location:user.php');
+        }
+
+        else{
+?>
+
 <?php include ("topbit.php");?>
 <?php include ('adminlogin.php');?>
 
@@ -25,9 +38,25 @@
                 <div id="tabcontent">
                     <div class="tabcontent active" id="Menu1">
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  method="post" >
-                        <h3>Username: <input class="modalinput" id="username" name="username"/></h3>
-                        <h3>Password: <input class="modalinput" id="password" name ="password"/></h3>
-                        <input class="loginbutton" type="submit" name="login" id="loginbutton" value="Login"/>
+
+                        <!-- username error-->
+                            <div class="<?php echo $user_error; ?> ">
+                                Please enter a username!
+                            </div>
+                        <h3>Username: <input class="modalinput <?php echo $user_field;?> " id="username" name="username"/></h3>
+                         <!-- password error-->
+                            <div class="<?php echo $pass_error; ?> ">
+                                Please enter a password!
+                            </div>
+                        <h3>Password: <input class="modalinput  <?php echo $pass_field;?> " id="password" name ="password"/></h3>
+
+                        <!-- error for inccorect password -->
+                            <div class="error-text">  
+                                <?php echo $passerror; ?>
+                                <br>
+                            </div>              
+                                      
+                            <input class="loginbutton" type="submit" name="login" id="loginbutton" value="Login"/>
                         </form>
                     </div>                    
 
@@ -40,3 +69,6 @@
 
 </body>
 </html>
+<?php
+        }//end if else
+?>
