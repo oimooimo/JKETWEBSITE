574c8fd4ef014e5ced96a2dfd1570df001044e06
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-06 19:31:56 +1200

    Update adduser.php
    Fixed some variables and also validation so that it would be able to submit/ go to add_usersuccess.php succesfully

--- htdocs/adduser.php
+++ htdocs/adduser.php
@@ -1,7 +1,5 @@
 <?php 
-
 //initialise form variables
-
 $userID="";
 $Username="";
 $Password="";
@@ -14,28 +12,28 @@ $Romlast= "";
 $First= "";
 $Last= "";
 
-$has_errors = "no";
+$has_usererrors = "no";
 
 // set up error field colours /visibility (no eroors at first)
-$user_error = $pass_error = $address_error = $name_error  = $account_error = $email_error = $phone_error= $rom_error = $jpn_error = "no-error";
+$user_error = $pass_error = $address_error = $name_error  = $account_error = $email_error = $phone_error= $rom_error = $jpn_error = $user_dupeerror = "no-error";
 
-$user_error = $pass_error = $address_error = $name_error  = $account_error = $email_error = $phone_error= $rom_error = $jpn_error = "form-ok";
+$user_error = $pass_error = $address_error = $name_error  = $account_error = $email_error = $phone_error= $rom_error = $jpn_error  = $user_dupeerror = "form-ok";
 
 //code executes below when the form is submited
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ if (isset($_POST['submituser'])) {
     
      //Get values from form
     $Username=mysqli_real_escape_string($dbconnect,$_POST['Username']);
 
     $Password=mysqli_real_escape_string($dbconnect,$_POST['Password']);
 
-    $Account=mysqli_real_escape_string($dbconnect,$_POST['Password']);
+    $Account=mysqli_real_escape_string($dbconnect,$_POST['Account']);
 
-    $Email=mysqli_real_escape_string($dbconnect,$_POST['Password']);
+    $Email=mysqli_real_escape_string($dbconnect,$_POST['Email']);
 
-    $Phone=mysqli_real_escape_string($dbconnect,$_POST['Password']);
+    $Phone=mysqli_real_escape_string($dbconnect,$_POST['Phone']);
 
-    $Address=mysqli_real_escape_string($dbconnect,$_POST['Password']);
+    $Address=mysqli_real_escape_string($dbconnect,$_POST['Address']);
 
     $Romfirst=mysqli_real_escape_string($dbconnect,$_POST['Romfirst']);
 
@@ -49,7 +47,7 @@ $user_error = $pass_error = $address_error = $name_error  = $account_error = $em
 
     // check username isnt empty
     if($Username == ""){
-        $has_errors ="yes";
+        $has_usererrors ="yes";
         $user_error = "error-text";
         $user_field="form-error";
     }
@@ -57,104 +55,104 @@ $user_error = $pass_error = $address_error = $name_error  = $account_error = $em
     //check that username doeesnt already exist
     $usercheck=mysqli_query($dbconnect, "SELECT Username FROM Users where Username = '$Username' ");
         if(mysqli_num_rows($usercheck) > 0){
-        $has_errors ="yes";
+        $has_usererrors ="yes";
         $user_dupeerror = "error-text";
         $user_dupefield="form-error";
     }
 
     // check that password isnt empty
     if($Password == "" ){
-        $has_errors ="yes";
+        $has_usererrors ="yes";
         $pass_error = "error-text";
         $pass_field="form-error";
     }
 
     //check that account isnt empty
     if($Account == "" ){
-        $has_errors ="yes";
+        $has_usererrors ="yes";
         $account_error = "error-text";
         $account_field="form-error";
     }
     
     //check that email isnt empty and is a valid email
     if($Email ==""){
-        $has_errors = "yes";
+        $has_usererrors = "yes";
         $email_error = "error-text";
         $email_field="form-error";
     }
     
-    // check that Pub Year is a valid reasonable date (no larger than 5 numbers long)
-    if(!ctype_digit($phone)||strlen($phone)>9||strlen($phone)<9){
-        $has_errors ="yes";
+
+    // check that phone Year is a valid rnumber  and is 9 digits long
+    if(!ctype_digit($Phone) || strlen($Phone) == 9 ){
+        $has_usererrors ="yes";
         $phone_error = "error-text";
         $phone_field="form-error";
     }
 
     //Check that address isnt empty
     if($Address ==""){
-        $has_errors ="yes";
+        $has_usererrors ="yes";
         $address_error = "error-text";
         $address_field="form-error";
         $description = "";
     }
 
     // CHeck that names are not empyty
+    //ctype checks what character set the variables are if not ctype then it means its not alphanumeric.
 
-    if($Romfirst == ""){
-        $has_errors ="yes";
+    if($Romfirst == "" ||!ctype_alnum($Romfirst)){
+        $has_usererrors ="yes";
         $rom_error = "error-text";
         $rom_field="form-error";
     }
     
-    if($Romlast == "" ){
-        $has_errors ="yes";
+    if($Romlast == ""||!ctype_alnum($Romlast) ){
+        $has_usererrors ="yes";
         $rom_error = "error-text";
         $rom_field="form-error";
     }
     
-    if($First == ""){
-        $has_errors ="yes";
+    if($First == ""||ctype_alnum($First)){
+        $has_usererrors ="yes";
         $jpn_error = "error-text";
         $jpn_field="form-error";
     }
     
-    if($Last == ""){
-        $has_errors ="yes";
+    if($Last == ""||ctype_alnum($Last)){
+        $has_usererrors ="yes";
         $jpn_error = "error-text";
         $jpn_field="form-error";
     }
     
 
     //if there are no errors
-    if($has_errors =="no") {
-        
-        header('location: add_usersuccess.php');
+    if($has_usererrors =="no") {
+    header('location: add_usersuccess.php');
 
     // hide error text
        // add entry to database
-    $addentry_sql="INSERT INTO `Users` (`userID`, `Username`, `Password`, `Account`, `Email`, `Phone`, `Address`, `Romfirst`, `Romlast`, `First`, `Last`) VALUES (NULL, '', '', '', '', '', '', '', '', '', '');";
-    $addentry_query=mysqli_query($dbconnect,$addentry_sql);
+    $adduserentry_sql="INSERT INTO `Users` ( `userID`,`Username`, `Password`, `Account`, `Email`, `Phone`, `Address`, `Romfirst`, `Romlast`, `First`, `Last`) VALUES (NULL, '$Username', '$Password', '$Account', '$Email', '$Phone', '$Addresss', '$Romfirst', '$Romlast', '$First', '$Last');";
+    $adduserentry_query=mysqli_query($dbconnect,$adduserentry_sql);
 
-    // get od fpr mext page
-    $getuserid_sql = "SELECT * FROM `User` WHERE
-     `userID` = $userID
+    // get ID fpr mext page
+    $getuserid_sql = "SELECT * FROM `Users` WHERE
+    `Username` LIKE '$Username' 
     ";
     $getuserid_query=mysqli_query($dbconnect,$getuserid_sql);
     $getuserid_rs=mysqli_fetch_assoc($getuserid_query);
 
     $userID = $getuserid_rs['userID'];
     $_SESSION['userID']=$userID;
-
     }// end of no errors if
 
  }
-?>
+?> 
 
         <div class="add-entry">
 
             <h3>Add An Entry</h3>
 
-                <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" target="_blank" id="form">
+                <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" id="form">
                 
 
                 <!-- User Name (Required) -->
@@ -207,7 +205,9 @@ $user_error = $pass_error = $address_error = $name_error  = $account_error = $em
                         Please enter and Address
                     </div>
                 <textarea class="add-field <?php echo address_field?> address p" type="text" name="Address" value="<?php echo $Address; ?>"  placeholder="Address" rows = "2" cols = "40"></textarea>
-
+                    
+                    
+                <div>
                  <!-- Romfirst (Required) -->
                     <!--error message-->
                     <div class="<?php echo $rom_error; ?> ">
@@ -221,7 +221,10 @@ $user_error = $pass_error = $address_error = $name_error  = $account_error = $em
                         Please enter a English Last name
                     </div>
                 <input class="add-field add-rom <?php echo $rom_field;?>  p" type="text" name="Romlast" value="<?php echo $Romlast; ?>"  placeholder="English last name (required)..." />
-
+                </div>
+                
+                
+                <div>
                  <!-- First (Required) -->
                     <!--error message-->
                     <div class="<?php echo $jpn_error; ?> ">
@@ -236,8 +239,10 @@ $user_error = $pass_error = $address_error = $name_error  = $account_error = $em
                     </div>
                 <input class="add-field add-jpn <?php echo $jpn_field;?>  p" type="text" name="Last" value="<?php echo $Last; ?>"  placeholder="Japanese Last name(required)..." />
                 <!-- SUbmit Button-->
+                </div>
 
-               <button class="submit tabs advanced-button" type="submit" value="Submit" id="Submit"  > Submit </button>
+                
+               <button class="submit tabs advanced-button" type="submit" value="submituser" name="submituser"> Submit </button>
             
         </form>
         </div>  <!---/addentry-->
