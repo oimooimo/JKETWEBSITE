dbbbc328573da10064bd2ec1bfea71d53bd96c72
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-06 21:13:28 +1200

    Create admin1.php
    New admin1.php page because form doesnt seem to work well with two forms on one page

---
+++ htdocs/admin1.php
@@ -0,0 +1,63 @@
+<?php 
+    session_start();
+    if(($_SESSION["login"]) == "admin"){
+        
+        
+    include ("topbit.php");
+?>
+     
+    <link rel="stylesheet" href="/css/loginstyle.css">
+<div class="wrapper">
+<div class="search">
+        <img src="image/search.JPG" alt="searchphoto" style="height: 200px;" class="center"/>
+
+        </div>
+            <div class="menu box">
+                        
+                        <a href="logout.php"> Logout </a>
+
+                        <a href="search.php">
+                        <img src="image/icon.png" id="myBtn" href="search.php"></img>
+                        </a>
+            </div>		
+            <div class="maincontent">
+
+            <div class="aside box">
+                <div id="tabs">
+                <a class="tabs" href="admin.php">Book Addition</a>
+                <a class="tabs" href="#Menu2">User Addition</a>
+                <a class="tabs" href="#Menu3">User List</a>
+                <a class="tabs" href="#Menu4">Delete</a>
+
+                </div><!--tabs-->
+            </div> <!--/aside box-->
+
+            <div class="content1 box">
+                <div id="tabcontent">
+
+                    <div class="tabcontent active" id="Menu2">
+                    <?php include 'adduser.php' ?>
+                    </div>
+
+                    <div class="tabcontent" id="Menu3">
+                        <?php include 'userlist.php' ?>
+                    </div>
+                </div><!-- end of tB CONTENT    -->
+
+            </div> <!-- end of content1 -->
+
+        </div> <!-- end of main-->
+    </div>
+
+</body>
+</html>
+<?
+    }// end of Admin session if
+    else{
+        header('Location:login.php');
+         exit();
+
+    }
+     
+
+    ?>
