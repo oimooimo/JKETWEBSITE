ec6ce0d5abf7c192461eb6f078e36a616fc5c67f
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-11 18:12:04 +1200

    Update userinfo.php

--- htdocs/userinfo.php
+++ htdocs/userinfo.php
@@ -5,8 +5,9 @@
     $find_usersql = "SELECT * FROM `Users`
     WHERE `userID` = $userID
         ";
-    $find_userquery = mysqli_query($dbconnect, $find_usersql);
+   $find_userquery = mysqli_query($dbconnect, $find_usersql);
+    $find_userrs =  mysqli_fetch_assoc($find_userquery);
     $usercount = mysqli_num_rows($find_userquery)
-
+    
     ?>
 <?php include 'userresults.php' ?>
