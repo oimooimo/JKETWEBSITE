1d6be64fdcd3a0f5597fec2ed958312162fab3c8
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-11 18:13:00 +1200

    Update userresults.php

--- htdocs/userresults.php
+++ htdocs/userresults.php
@@ -1,4 +1,4 @@
-  <?php 
+    <?php 
 
 if($usercount < 1) {
 
@@ -6,7 +6,8 @@ if($usercount < 1) {
 
 <div class="error">
 
-    Sorry! There are no results that match your search. Please use the search box in the right for a more detailed search!
+    Sorry! There are no results that match your search. Please use the search box in the right for a more detailed search!<br>
+ごめんなさい！検索に一致する結果はありません。右側の検索ボックスを使用して、より詳細な検索を行ってください
 
 </div><!-- end error -->
 
@@ -36,20 +37,20 @@ else {
             
             <?php 
                 if ($find_userrs['Account'] == 1) {?>
-                    <div style="color:red;"> Admin </div>
+                    <div style="color:red;"> Admin・アドミン </div>
 
                 <?php
                 } //end Check in if
 
                 elseif ($find_userrs['Account'] == 2){
 
-                ?> <div style="color:green;"> Regular member </div>
+                ?> <div style="color:green;"> Regular member・普通メンバー </div>
                 <?php
                 } // end check in else
 
                 elseif($find_userrs['Account'] == 3){
 
-                ?> <div style="color:darkgreen;"> Playgroup member </div>
+                ?> <div style="color:darkgreen;"> Playgroup member・プレイグループメンバー </div>
                 <?php
                 } // end ch
 
@@ -58,19 +59,19 @@ else {
         </div> <!--/flexcontainer-->
 
         <div>
-            <b>Username:</b>
+            <b>Username・ユーザーネーム:</b>
             <?php echo $find_userrs['Username']; ?>
 
             <br />
-            <b> Email:</b>
+            <b> Email・メール:</b>
             <?php echo $find_userrs['Email']; ?>
 
             <br />
-            <b> Phone number:</b>
+            <b> Phone number・電話番号:</b>
             <?php echo $find_userrs['Phone']; ?>
 
             <br />
-            <b> Address:</b>
+            <b> Address・住所:</b>
             <?php echo $find_userrs['Address']; ?>
         </div> <!--- Sub information-->
      
