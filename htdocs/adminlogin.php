e5e4faae4cbec1c40885e49397ea62083b5a06cd
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-08-11 18:17:44 +1200

    Update adminlogin.php

--- htdocs/adminlogin.php
+++ htdocs/adminlogin.php
@@ -20,17 +20,16 @@ $passerror = "";
         $pass_field="form-error";
     }
 
-    if(isset ($_REQUEST['login'])){
+    if(isset ($_REQUEST['loginbutton'])){
 
         $login_sql="SELECT * FROM `Users` WHERE Username='$username' AND Password='$password'";
         $login_query=mysqli_query($dbconnect,$login_sql);
 
         // if user is admin then take them to admin panel and give their session admin // mrs greenwood from here  https://stackoverflow.com/questions/22340304/how-to-make-login-form-with-admin-and-user-in-php/22340649
         if((mysqli_num_rows($login_query)>0)&& ($has_errors=="no")){
-            
 
                 $login_row=mysqli_fetch_array($login_query);
-                    if ($login_row['Account']== 1){
+                    if ($login_row['Account'] == 1){
                     $_SESSION["login"]= "admin";
                     header('location:admin.php');
                     }// end of if
@@ -38,13 +37,14 @@ $passerror = "";
                 //if user is not an admin then give them a user session and take them to user panel
                 else{
                     $_SESSION['login']= "user";
-                    echo "No";
+                    $_SESSION['userID']= ($login_row['userID']);
+                    header('location:user.php');
                 }// end of else
 
         }//end of main if
 
         else{ //pasword error
-        $passerror="incorrect username and/or password";
+        $passerror="incorrect username and/or password ユーザー名またはパスワードが正しくありません";
         }
     }
 
